f3d3a3bbd0e48dfc0537eb5ba2b2c9e24da44456
SandyRamadhan28 <ramadhansandy388@gmail.com>
2024-01-03 21:51:11 +0700

    upload file

---
+++ user/user.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
+    exit();
+}
+
+include '../class/koneksi.php';
+
+$userQuery = "SELECT * FROM users WHERE id=" . $_SESSION["user_id"];
+$userResult = mysqli_query($koneksi, $userQuery);
+$user = mysqli_fetch_assoc($userResult);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Iuran KAS RT</title>
+    <link rel="stylesheet" type="text/css" href="css/user.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <h1>Sistem Iuran KAS RT</h1>
+            <p>Selamat datang, <?php echo $user["nama"]; ?>!</p>
+        </header>
+
+        <div class="wrapper">
+            <div class="side-bar">
+                <nav>
+                    <ul>
+                        <li><a href="#data-warga">Data Warga</a></li>
+                        <li><a href="#transaksi_iuran_user">Iuran KAS</a></li>
+                        <li><a href="#laporan_transaksi_user">Laporan Transaksi</a></li>
+                        <li><a href="#belum_bayar_user">Belum Bayar Iuran</a></li>
+                        <li><a href="#jumlah_kas_user">Jumlah KAS</a></li>
+                        <li><a href="../logout.php">Logout</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            
+        </div>
+
+        <footer>
+            <p>&copy; 2024, Teknik Informatika, Universitas Pelita Bangsa, Sistem Iuran Kas RT</p>
+        </footer>
+    </div>
+
+
+</body>
+</html>
